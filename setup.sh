732510a8ea14c7b2ec062641a807cb84ca275db5
zanderhavgaard <zander@havgaard.dk>
2019-11-03 13:19:44 +0100

    updated setup script

--- setup.sh
+++ setup.sh
@@ -5,9 +5,9 @@ echo -e "
 "
 
 echo -e "\nSymlinking .emacs ..."
-mkdir /home/$USER/.emacs.d &> /dev/null
-rm /home/$USER/.emacs # sorry not sorry...
-ln -sv /home/$USER/emacs-config/.emacs /home/$USER/.emacs
+mkdir /home/zander/.emacs.d &> /dev/null
+rm /home/zander/.emacs # sorry not sorry...
+ln -sv /home/zander/emacs-config/.emacs /home/zander/.emacs
 
 echo -e "\nRunning Emacs to finish installation!"
 echo -e "After all of the packages are installed run: M-x all-the-icons-install-fonts to install icon fonts"
